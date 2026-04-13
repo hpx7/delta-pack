4302b6aa8d3664f9e49583c47b3fde57a602e302
Harsh Pandey <hpandey793@gmail.com>
2026-04-12 20:24:56 -0400

    revamp docs site to be statically built, rename to website

---
+++ website/src/main.ts
@@ -0,0 +1,9 @@
+import { marked } from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/github.css";
+import "./styles.css";
+import readme from "../../README.md?raw";
+
+const target = document.getElementById("readme")!;
+target.innerHTML = marked.parse(readme) as string;
+hljs.highlightAll();
